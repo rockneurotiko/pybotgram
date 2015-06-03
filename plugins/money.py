968f6935f118d83b1eb1c43f48b457c37d394911
Rock Neurotiko <miguelglafuente@gmail.com>
2015-06-03 18:39:24 +0200

    fixes to the money plugin

--- plugins/money.py
+++ plugins/money.py
@@ -1,31 +1,36 @@
-import urllib2
+import requests
 import re
 
-def run(msg, matches):
-	currencies = ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTC", "BTN", "BWP", "BYR", "BZD", "CAD", "CDF", "CHF", "CLF", "CLP", "CNH", "CNY", "COP", "CRC", "CUP", "CVE", "CZK", "DEM", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FIM", "FJD", "FKP", "FRF", "GBP", "GEL", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "IEP", "ILS", "INR", "IQD", "IRR", "ISK", "ITL", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LTL", "LVL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKG", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "STD", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VEF", "VND", "VUV", "WST", "XAF", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMK", "ZMW", "ZWL"]
-	fromCur = matches[0].upper()
-	amount  = matches[1]
-	toCur   = matches[2].upper()
-	
-	if not fromCur in currencies:
-		return "ERROR: currency \"" + fromCur + "\" is not recognized"
-	
-	if not toCur in currencies:
-		return "ERROR: currency \"" + toCur + "\" is not recognized"
-	
-	returnText = "Oops, an error occurred."
 
-	url = "https://www.google.com/finance/converter?a=" + amount + "&from=" + fromCur + "&to=" + toCur 	
-	html = urllib2.urlopen(url).read()
-	moneyMatches = re.search("<span class=bld>([\d.]+)", html)
-	if moneyMatches:
-		returnText = "{} {} is {} {}".format(amount, fromCur, moneyMatches.group(1), toCur)
-	
-	return returnText
+def run(msg, matches):
+    currencies = ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTC", "BTN", "BWP", "BYR", "BZD", "CAD", "CDF", "CHF", "CLF", "CLP", "CNH", "CNY", "COP", "CRC", "CUP", "CVE", "CZK", "DEM", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FIM", "FJD", "FKP", "FRF", "GBP", "GEL", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "IEP", "ILS", "INR", "IQD", "IRR", "ISK", "ITL", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD",
+                  "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LTL", "LVL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKG", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "STD", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VEF", "VND", "VUV", "WST", "XAF", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMK", "ZMW", "ZWL"]
+    fromCur = matches[0].upper()
+    amount = matches[1]
+    toCur = matches[2].upper()
+
+    if fromCur not in currencies:
+        return "ERROR: currency \"" + fromCur + "\" is not recognized"
+
+    if toCur not in currencies:
+        return "ERROR: currency \"" + toCur + "\" is not recognized"
+
+    returnText = "Oops, an error occurred."
+
+    url = "https://www.google.com/finance/converter?a=" + \
+        amount + "&from=" + fromCur + "&to=" + toCur
+    r = requests.get(url)
+    html = r.text
+    moneyMatches = re.search("<span class=bld>([\d.]+)", html)
+    if moneyMatches:
+        returnText = "{} {} is {} {}".format(
+            amount, fromCur, moneyMatches.group(1), toCur)
+
+    return returnText
 
 __info__ = {
     "description": "Currency converter",
-    "usage": ["!m[oney] (from currency) (amount) (to currency)"],
+    "usage": ["!money (from currency) (amount) (to currency)"],
     "patterns": ["^!m(?:oney)? (\w+) ([\d\.]+) (\w+)$"],
     "run": run
 }
