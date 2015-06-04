9f5786d151ad644106ef068777eeb6449d6ce5eb
Rock Neurotiko <miguelglafuente@gmail.com>
2015-06-04 14:11:50 +0200

    Async process of the request, precision support and PEP8 style

--- plugins/calc.py
+++ plugins/calc.py
@@ -1,20 +1,26 @@
-import requests
+from gl import utils
+
+
+def cb(r, receiver):
+    text = r.text if r.text else 'An error occurred.'
+    receiver.send_msg(text)
 
 
 def run(msg, matches):
-    returnText = "Oops, an error occurred."
+    receiver = utils.get_receiver(msg)
     exp = matches[0]
+    path = "http://api.mathjs.org/v1/"
     payload = {'expr': exp}
-    r = requests.get("http://api.mathjs.org/v1/", params = payload)
-    
-    if r.text:
-	    returnText = r.text
-	
-    return returnText
+    if len(matches) > 1:
+        payload.update({'precision': matches[1]})
+    gcb = utils.gac(cb, receiver)
+    utils.mp_requests('GET', path, gcb, params=payload)
 
 __info__ = {
     "description": "A calculator to evaluate expressions",
     "usage": ["!calc (expression)"],
-    "patterns": ["^!calc ([\s\S]+)$"],
+    "patterns": [
+        "^!calc ([\s\S]+) prec (\d+)?$",
+        "^!calc ([\s\S]+)$"],
     "run": run
 }
